1e6f6741d7096dbec11279fab9fbe7f038c1edd1
Jack Crump-Leys <jackcrumpleys@gmail.com>
2026-07-17 11:25:52 +1200

    fix CI

--- tools/packet_inspector/build.rs
+++ tools/packet_inspector/build.rs
@@ -173,7 +173,7 @@ fn write_transformer(packets: &[Packet]) -> anyhow::Result<()> {
 
                 match_arms.extend(quote! {
                     chunkedge_protocol::packets::#lowercase_state::#name::ID => {
-                        chunkedge_protocol::packets::#lowercase_state::#name::decode(&mut data).map(|p| format!("{:#?}", p))
+                        chunkedge_protocol::packets::#lowercase_state::#name::decode(&mut data).map(|p| format!("{p:#?}"))
                     }
                 });
             }
